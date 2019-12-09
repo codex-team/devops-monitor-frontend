ab83140a88b5c4c80d8dd9453c5007bcc2377053
GeekaN <geekan@bk.ru>
2019-12-09 15:10:35 +0300

    Get server names

--- src/store/servers/actions.ts
+++ src/store/servers/actions.ts
@@ -8,7 +8,7 @@ import { RootState } from '../types';
  */
 const API_URL = 'https://api.devops.codex.so/projects';
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZGUwMTQyMjg1OGEyNDc4ZGVjZjYzMTkiLCJpYXQiOjE1NzQ5NjY1NDF9.8VMzdHhV45TAYxHq4tvkr_mX_6aOjB93v9u1yXCUnYA';
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZGU4ZTVmYWFhYWFlZTdkOWRmNjJhYmIiLCJpYXQiOjE1NzU4ODIwOTZ9.IGeqCLeLmkd7I6JGV8CoajyDVbj0aPXcIevaBt801gU';
 
 const actions: ActionTree<ServerModuleState, RootState> = {
   /**
@@ -18,8 +18,7 @@ const actions: ActionTree<ServerModuleState, RootState> = {
     const serversList = await axios.get(API_URL, {
       headers: { authorization: 'Bearer ' + TOKEN }
     });
-
-    commit('setServersList', serversList.data);
+    commit('setServersList', serversList.data.data);
   }
 };
 
