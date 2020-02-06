692ee0e4aaa7a486ed885e6eb85697452cfd5586
GeekaN <geekan@bk.ru>
2020-02-06 16:46:50 +0300

    Update request and parsing

--- src/store/servers/actions.ts
+++ src/store/servers/actions.ts
@@ -6,18 +6,19 @@ import { RootState } from '../types';
 /**
  * Get servers data from the back-end
  */
-const API_URL = 'https://api.devops.codex.so/';
+const API_URL = 'https://api.devops.codex.so';
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZGY3YzE3ODIyMTIwZDQ3M2ZjMzgwODciLCJpYXQiOjE1NzY3NTQ1NDZ9.W4h2YNVftXaoHi9yBCjaaiHvtS7EY32dGF-5ZE0BWYk';
-
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZTI2YTliNzVhYjIyNjQ4MzhlODI3YTUiLCJpYXQiOjE1ODA5MDUzMzB9.0NH1KFEnv8IuVZAlPcZ7LOfmfzTsRyaPNV6f8l0xhMI';
 const actions: ActionTree<ServerModuleState, RootState> = {
   /**
    * Fetch data about servers from API
    * Fetch services on every server
    */
   async fetchData({ commit }): Promise<void> {
-    const serversList = await axios.get(API_URL + 'projects', {
-      headers: { authorization: 'Bearer ' + TOKEN }
+    const serversList = await axios.get(API_URL + '/projects', {
+      headers: {
+        authorization: 'Bearer ' + TOKEN
+      }
     });
 
     var servers = serversList.data.data;
@@ -29,17 +30,26 @@ const actions: ActionTree<ServerModuleState, RootState> = {
     for (let i = 0; i < servers.length; i++) {
       console.log('Server token ', servers[i].token);
 
-      const service = await axios.get(API_URL + 'services', {
-        headers: {
-          authorization: 'Bearer ' + TOKEN
-        },
+      await axios.get(API_URL + '/services', {
         params: {
           projectToken: servers[i].token
+        },
+        headers: {
+          authorization: 'Bearer ' + TOKEN
         }
-      });
+      }).then(service => {
+        service = service.data; // get data with statusCode and other data
+        let payload = [];
 
-      console.log('Service.data ', JSON.stringify(service.data));
-      servers[i].services = service;
+        if (service.data.length != 0) {
+          // if we have useful payload
+          payload = service.data[0].payload;
+        } else {
+          // if data is empty
+          servers[i].services = {};
+        }
+        servers[i].services = payload;
+      });
     }
     commit('setServersList', servers);
   }
