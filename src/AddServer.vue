beceac8a7e876469f6ff7685f8c87ad1cc2eb9b9
GeekaN <geekan@bk.ru>
2020-02-13 16:05:01 +0300

    Integration token layout
    Added integration token layout to the AddServer page

--- src/AddServer.vue
+++ src/AddServer.vue
@@ -23,6 +23,32 @@
           Add server
         </button>
       </div>
+      <div class="servers-map__add-server">
+        <img
+          class="add-server__image"
+          src="./assets/koala_closed_eyes.svg"
+        >
+        <h2 class="add-server__header add-server__header--little-margin">
+          Done!
+        </h2>
+        <p class="add-server__text">
+          codex-kepler was added. Next you need to install the Collector script to your server.
+        </p>
+        <h3 class="add-server__small-header">
+          Integration token
+        </h3>
+        <p class="add-server__integration-token">
+          eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiI1ZTNhYjliOWNkNmI3ODAwMjM3OGEzNjUiLCJpYXQiOjE1ODA5MDY5Mzd9.MQXPqBMEhwRyadPteDMhvepK_sMw_tzNHNcpER7IODY
+        </p>
+        <a
+          class="add-server__guide"
+          href="#"
+        >Follow the installation guide</a>
+        <a
+          class="add-server__servers-list"
+          href="/"
+        >Go to servers list &raquo;</a>
+      </div>
     </div>
   </div>
 </template>
@@ -47,9 +73,10 @@ export default class App extends Vue {}
 
 <style>
 @import "~minireset.css/minireset.min.css";
+@import url('https://fonts.googleapis.com/css?family=PT+Mono&display=swap');
 
 .app {
-  font-family: Roboto, sans-serif;
+  font-family: 'Roboto', sans-serif;
   font-size: 13px;
   color: #000000;
   padding: 30px;
@@ -76,7 +103,7 @@ a {
   width: 450px;
   min-width: 250px;
   margin: 0 25px 25px 0;
-  padding: 60px 90px;
+  padding: 60px 70px;
 }
 
 .add-server__image {
@@ -108,6 +135,7 @@ a {
 }
 
 .add-server__button {
+  display: block;
   font-size: 14px;
   border-radius: 3px;
   padding: 10px;
@@ -116,5 +144,40 @@ a {
   background-color: #43b1f6;
   border: 0;
   cursor: pointer;
+}
+
+.add-server__header--little-margin {
+  margin-bottom: 10px;
+}
+
+.add-server__text {
+  font-size: 15px;
+  margin-bottom: 15px;
+}
+
+.add-server__integration-token {
+  font-family: 'PT Mono', 'Roboto', sans-serif;
+  width: 260px;
+  border-radius: 3px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+  font-size: 12px;
+  color: #712cab;
+  word-wrap: break-word;
+  padding: 9px 10px;
+  margin-bottom: 20px;
+}
+
+.add-server__guide {
+  display: inline-block;
+  font-size: 15px;
+  color: #0080d0;
+  border-bottom: 1px dashed #0080d0;
+  margin-bottom: 15px;
+}
+
+.add-server__servers-list {
+  display: block;
+  font-size: 15px;
+  color: rgba(0, 0, 0, 0.6);
 }
 </style>
